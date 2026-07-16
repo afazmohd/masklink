f3519f7e16860b5c1cdb00414ecf1837a74cd7d7
notme <afazmohd333@gmail.com>
2026-07-16 22:47:13 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-
-// load your html files
+// Load your HTML files
 app.use(express.static("./"));
-
 
 app.post("/shorten", async (req, res) => {
 
@@ -18,54 +15,40 @@ app.post("/shorten", async (req, res) => {
 
         const longUrl = req.body.url;
 
-
         const response = await fetch(
-            "https://api-ssl.bitly.com/v4/shorten",
-            {
-                method: "POST",
-
-                headers: {
-                    "Authorization":
-                    `Bearer ${process.env.BITLY_TOKEN}`,
-
-                    "Content-Type":
-                    "application/json"
-                },
-
-                body: JSON.stringify({
-                    long_url: longUrl
-                })
-            }
+            `https://is.gd/create.php?format=json&url=${encodeURIComponent(longUrl)}`
         );
 
-const data = await response.json();
+        const data = await response.json();
 
-console.log("Bitly response:", data);
+        console.log("is.gd response:", data);
 
-if (!response.ok) {
-    return res.status(response.status).json(data);
-}
+        if (data.errorcode) {
+            return res.status(400).json({
+                error: data.errormessage
+            });
+        }
 
-res.json({
-    shortUrl: data.link
-});
-        
-    
-    }catch(error) {
+        res.json({
+            shortUrl: data.shorturl
+        });
+
+    } catch (error) {
+
+        console.error(error);
 
         res.status(500).json({
-            error:"Bitly error"
+            error: "is.gd error"
         });
 
     }
 
 });
 
-
 app.listen(3000, () => {
 
-console.log(
-"MaskPhish running at http://localhost:3000"
-);
+    console.log(
+        "MaskPhish running at http://localhost:3000"
+    );
 
 });
