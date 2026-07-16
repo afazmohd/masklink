293a0e51cc646c6d2fd7729baa0d35c11ab42e86
notme <afazmohd333@gmail.com>
2026-07-16 21:09:45 +0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+// load your html files
+app.use(express.static("./"));
+
+
+app.post("/shorten", async (req, res) => {
+
+    try {
+
+        const longUrl = req.body.url;
+
+
+        const response = await fetch(
+            "https://api-ssl.bitly.com/v4/shorten",
+            {
+                method: "POST",
+
+                headers: {
+                    "Authorization":
+                    `Bearer ${process.env.BITLY_TOKEN}`,
+
+                    "Content-Type":
+                    "application/json"
+                },
+
+                body: JSON.stringify({
+                    long_url: longUrl
+                })
+            }
+        );
+
+
+        const data = await response.json();
+
+
+        res.json({
+            shortUrl: data.link
+        });
+
+
+    } catch(error) {
+
+        res.status(500).json({
+            error:"Bitly error"
+        });
+
+    }
+
+});
+
+
+app.listen(3000, () => {
+
+console.log(
+"MaskPhish running at http://localhost:3000"
+);
+
+});
